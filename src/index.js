a840fc0babbfb63c955e18848f1ce2a52febc96f
jeanzhou31 <jeanzhou31@gmail.com>
2016-07-07 02:25:27 -0400

    made fixes

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import $ from 'jquery';
 import './style.scss';
 
 let num = 0;
-setInterval(function () {
-  ++num;
-  $('#main').html('You\'ve been on this page for ' + num + ' seconds');
-}, 1000);
+function count() {
+  num++;
+  $('#main').html(`You've been on this page for ${num} seconds`);
+}
+setInterval(count, 1000);
